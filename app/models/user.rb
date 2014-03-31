0036b60167574ab800bd652e439d3b5abed0dc08
Ian White <ianwhite@utexas.edu>
2014-03-30 20:51:44 -0500

    Able to authorize app on Github.  Returns to site saying invalid credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:github]
   validates_presence_of :name
 
   def self.create_with_omniauth(auth)
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 RailsOmniauthGithub::Application.routes.draw do
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => "home#index"
   resources :users, :only => [:index, :show, :edit, :update ]
-  get '/auth/:provider/callback' => 'sessions#create'
-  get '/signin' => 'sessions#new', :as => :signin
-  get '/signout' => 'sessions#destroy', :as => :signout
-  get '/auth/failure' => 'sessions#failure'
+  # get '/auth/:provider/callback' => 'sessions#create'
+  # get '/signin' => 'sessions#new', :as => :signin
+  # get '/signout' => 'sessions#destroy', :as => :signout
+  # get '/auth/failure' => 'sessions#failure'
 end
