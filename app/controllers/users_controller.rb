daf27e0dbdf7a1bda7a555bec76ede028f0f5165
Ian White <ianwhite@utexas.edu>
2014-03-30 21:24:06 -0500

    Remove custom authentication and session methods, replace with devise.  Change logout path in navigation partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :correct_user?, :except => [:index]
+  # before_filter :correct_user?, :except => [:index]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
 
 
@@ -28,6 +29,9 @@ class UsersController < ApplicationController
   end
 
   private
+  def set_user
+      @user = User.find(params[:id])
+    end
 
   def secure_params
     params.require(:user).permit(:email)
